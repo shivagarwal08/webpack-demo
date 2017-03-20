873d77c81fb95bf341b2760d60b8ce00260d956c
Shiv Kumar Agarwal <Shiv.Agarwal@rsystems.com>
2017-03-20 12:55:00 +0530

    style-loader & css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
-// var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+        path: 'dist',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.css/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Demo Dynamic Title',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/index.html'
+        })
+    ]
 }
